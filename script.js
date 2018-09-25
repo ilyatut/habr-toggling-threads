ef707010ec397c0dd706378c1131ac2a77d5899f
Ilya Tyumentsev <ilyatut@gmail.com>
2018-09-26 01:05:48 +0500

    Add open/close for all comments

--- script.js
+++ script.js
@@ -6,22 +6,28 @@ $(document).ready(function(){
 		'padding: 2px 7px',
 		'margin-left: 5px',
 		'color: #fff',
-		'background: #bbcdd6',
+		'background: #8fafbf',
 		'cursor: pointer',
 		'border-radius: 3px',
 		'text-align: center',
 		'opacity: 0.3',
-		'font-weight: bold',
+		'font-weight: normal',
 	].join("; ");
 	
 	// create all buttons
 	var btn_html = '<span class="switch_btn" style="' + btn_style + ';">-</span>';
-	var close_first_level_btn = '<span class="close_all" style="' + btn_style + '; margin-top: 4px;">свернуть все</span>';
-	var open_first_level_btn = '<span class="open_all" style="' + btn_style + '; margin-top: 4px;">развернуть все</span>';
+	var close_first_level_btn = '<span class="close_first_level" style="' + btn_style + '; margin-top: 4px;">- 1ый</span>';
+	var open_first_level_btn = '<span class="open_first_level" style="' + btn_style + '; margin-top: 4px;">+ 1ый</span>';
+	var close_all_btn = '<span class="close_all" style="' + btn_style + '; margin-top: 4px;">- все</span>';
+	var open_all_btn = '<span class="open_all" style="' + btn_style + '; margin-top: 4px;">+ все</span>';
 	
 	$('.comment__head').append(btn_html);
-	$('.comments-section__subscribe-panel').append(close_first_level_btn);
-	$('.comments-section__subscribe-panel').append(open_first_level_btn);
+	$('.comments-section__subscribe-panel')
+		.append(close_first_level_btn)
+		.append(open_first_level_btn)
+		.append(close_all_btn)
+		.append(open_all_btn)
+	;
 	
 	// create open/close event for single comments
 	$(document).on('click', '.switch_btn', function(){
@@ -40,7 +46,7 @@ $(document).ready(function(){
 	});
 
 	// create close event for the first level of all comments
-	$(document).on('click', '.close_all', function(){
+	$(document).on('click', '.close_first_level', function(){
 		var first_level_comments = $("#comments-list > .content-list__item_comment");
 
 		$.each(first_level_comments, function(index, value) {
@@ -53,10 +59,36 @@ $(document).ready(function(){
 	});
 	
 	// create open event for the first level of all comments
-	$(document).on('click', '.open_all', function(){
+	$(document).on('click', '.open_first_level', function(){
 		var first_level_comments = $("#comments-list > .content-list__item_comment");
 		
 		$.each(first_level_comments, function(index, item) {
+			var button = $(this).find(".switch_btn").filter(':first');
+			var opened = button.hasClass('opened') || !button.hasClass('closed');
+			if (!opened) {
+				button.click();	
+			}
+		});
+	});
+
+	// create close event for all comments
+	$(document).on('click', '.close_all', function(){
+		var all_comments = $(".content-list__item_comment");
+
+		$.each(all_comments, function(index, value) {
+			var button = $(this).find(".switch_btn").filter(':first');
+			var opened = button.hasClass('opened') || !button.hasClass('closed');
+			if (opened) {
+				button.click();	
+			}
+		});
+	});
+	
+	// create open event for all comments
+	$(document).on('click', '.open_all', function(){
+		var all_comments = $(".content-list__item_comment");
+		
+		$.each(all_comments, function(index, item) {
 			var button = $(this).find(".switch_btn").filter(':first');
 			var opened = button.hasClass('opened') || !button.hasClass('closed');
 			if (!opened) {
