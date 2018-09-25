fdbb4a70f257f6ccff7274f2c7b2a8a4d141c715
Ilya Tyumentsev <ilyatut@gmail.com>
2018-09-26 00:39:23 +0500

    Add buttons for collapsing first level comments

--- script.js
+++ script.js
@@ -16,9 +16,14 @@ $(document).ready(function(){
 	
 	// create all buttons
 	var btn_html = '<span class="switch_btn" style="' + btn_style + ';">-</span>';
-	$('.comment__head').append(btn_html);
+	var close_first_level_btn = '<span class="close_all" style="' + btn_style + '; margin-top: 4px;">свернуть все</span>';
+	var open_first_level_btn = '<span class="open_all" style="' + btn_style + '; margin-top: 4px;">развернуть все</span>';
 	
-	// create open/close event
+	$('.comment__head').append(btn_html);
+	$('.comments-section__subscribe-panel').append(close_first_level_btn);
+	$('.comments-section__subscribe-panel').append(open_first_level_btn);
+	
+	// create open/close event for single comments
 	$(document).on('click', '.switch_btn', function(){
 		var sub_comments_block = $(this).closest('.content-list__item').children('.content-list');
 		var opened = $(this).hasClass('opened') || !$(this).hasClass('closed');
@@ -33,5 +38,31 @@ $(document).ready(function(){
 			$(this).removeClass('closed').addClass('opened').html('-');
 		}
 	});
+
+	// create close event for the first level of all comments
+	$(document).on('click', '.close_all', function(){
+		var first_level_comments = $("#comments-list > .content-list__item_comment");
+
+		$.each(first_level_comments, function(index, value) {
+			var button = $(this).find(".switch_btn").filter(':first');
+			var opened = button.hasClass('opened') || !button.hasClass('closed');
+			if (opened) {
+				button.click();	
+			}
+		});
+	});
 	
+	// create open event for the first level of all comments
+	$(document).on('click', '.open_all', function(){
+		var first_level_comments = $("#comments-list > .content-list__item_comment");
+		
+		$.each(first_level_comments, function(index, item) {
+			var button = $(this).find(".switch_btn").filter(':first');
+			var opened = button.hasClass('opened') || !button.hasClass('closed');
+			if (!opened) {
+				button.click();	
+			}
+		});
+	});
+
 });
