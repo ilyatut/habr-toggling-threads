54e840420c3b12e49f5928919ea459ff95e9b4ee
Ilya Tyumentsev <ilyatut@gmail.com>
2018-09-26 09:52:36 +0500

    Add unique prefix to inserted elements

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ $(document).ready(function(){
 		'text-align: center',
 		'opacity: 0.3',
 		'font-weight: normal',
-	].join("; ");
+	].join("; ") + ";";
 	
 	// create all single comment buttons
-	var btn_html = '<span class="switch_btn" style="' + btn_style + ';">-</span>';
+	var btn_html = '<span class="btt__switch_btn" style="' + btn_style + '">-</span>';
 	$('.comment__head').append(btn_html);
 
 	// create global control buttons
-	btn_style += '; margin-top: 4px';
+	btn_style += ' margin-top: 4px;';
 	var global_btns = 
-		'<span class="close_first_level" style="' + btn_style + ';">- 1ый</span>' + 
-		'<span class="open_first_level" style="' + btn_style + ';">+ 1ый</span>' + 
-		'<span class="close_all" style="' + btn_style + ';">- все</span>' + 
-		'<span class="open_all" style="' + btn_style + ';">+ все</span>'
+		'<span class="btt__close_first_level" style="' + btn_style + '">- 1ый</span>' + 
+		'<span class="btt__open_first_level" style="' + btn_style + '">+ 1ый</span>' + 
+		'<span class="btt__close_all" style="' + btn_style + '">- все</span>' + 
+		'<span class="btt__open_all" style="' + btn_style + '">+ все</span>'
 	;
 	$('.comments-section__subscribe-panel').append(global_btns);
 	
 	// create open/close event for single comments
-	$(document).on('click', '.switch_btn', function(){
+	$(document).on('click', '.btt__switch_btn', function(){
 		var sub_comments_block = $(this).closest('.content-list__item').children('.content-list');
 		var opened = $(this).hasClass('opened') || !$(this).hasClass('closed');
 	
@@ -45,28 +45,28 @@ $(document).ready(function(){
 	});
 
 	// create close event for the first level of all comments
-	$(document).on('click', '.close_first_level', function(){
+	$(document).on('click', '.btt__close_first_level', function(){
 		$("#comments-list > .content-list__item_comment").each(function() {
 			click_button($(this), true);
 		});
 	});
 	
 	// create open event for the first level of all comments
-	$(document).on('click', '.open_first_level', function(){
+	$(document).on('click', '.btt__open_first_level', function(){
 		$("#comments-list > .content-list__item_comment").each(function() {
 			click_button($(this), false);
 		});
 	});
 
 	// create close event for all comments
-	$(document).on('click', '.close_all', function(){
+	$(document).on('click', '.btt__close_all', function(){
 		$(".content-list__item_comment").each(function() {
 			click_button($(this), true);
 		});
 	});
 	
 	// create open event for all comments
-	$(document).on('click', '.open_all', function(){
+	$(document).on('click', '.btt__open_all', function(){
 		$(".content-list__item_comment").each(function() {
 			click_button($(this), false);
 		});
@@ -74,7 +74,7 @@ $(document).ready(function(){
 
 	// click on a button in a comment block (closed or open)
 	function click_button(element, find_opened) {
-		var button = element.find(".switch_btn").filter(':first');
+		var button = element.find(".btt__switch_btn").filter(':first');
 		var opened = button.hasClass('opened') || !button.hasClass('closed');
 		if ((find_opened && opened ) || (!find_opened && !opened)) {
 			button.click();	
